b3e93a75fd0192e93d2cd7d49b6e535e8f467073
eneski <eeneskilicaslan@gmail.com>
2018-08-10 13:21:31 +0300

    Add Treasure model

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-
+from .models import Treasure
 
 
 # Create your views here.
 
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
-
-
-class Treasure:
-    def __init__(self, name, value, material, location):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-
 
 treasures = [
         Treasure('Gold Nugget', 240, 'Gold', 'LA'),
